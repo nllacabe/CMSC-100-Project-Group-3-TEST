1c57871db9d102becd7218b9a7b1e1b41900ff34
adpaleracio <adpaleracio@up.edu.ph>
2024-05-10 15:23:55 +0800

    ui: Enhanced prototype layout

--- try-with-react/src/pages/Shop.jsx
+++ try-with-react/src/pages/Shop.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 export default function Shop() {
 
   const [ items, setItems ] = useState([])
+  const [totalItems, setTotal] = useState(0);             // useState for totalItems (initial is 0)
+  const [cart, setCart] = useState([]);                   // useState for cart (initial is an empty list)
 
   useEffect(() => {
     fetch('http://localhost:3001/get-all-products')
@@ -13,21 +15,68 @@ export default function Shop() {
       })
   })
 
+  function addToCart(item) {                    // add to cart function
+    setTotal(totalItems+1);                     // increments totalItems
+
+    let flag = 0;
+    for(const product of cart){                 // iterates through the cart list
+      if(product.productID == item.productID){                // if the item is already in the list
+        flag++;                                 // increments flag
+        product.count++;                        // increments the product count
+        break;
+      }
+    }
+    if(flag == 0){                                    // if the item is not in the list yet
+      const countField = {...item, count: 1};         // adds a new field to the object (count: 1)
+      setCart([...cart, countField]);                 // appends the object to the cart list
+    }
+  }
+
+  function deleteFromCart(deleteItem) {           // delete function
+    setTotal(totalItems-deleteItem.count);                              // subtracts the item count from the total item count
+    const newCart = cart.filter(item => item.productID !== deleteItem.productID);     // filters out the item to be deleted
+    setCart(newCart);
+  }
+
+
+
   return (
     <>
       <main>
         <div className="item-part">
           <div className="card-container">
-            {items.map((item, i) =>
+            {items.map((item) =>
                 <div className="card">
                   <p>{item.productName}</p>
                   <p>₱{item.productPrice}.00</p>
+                  <button onClick={() => addToCart(item)}>Add To Cart</button>
+                </div>
+            )}
+            {items.map((item) =>
+                <div className="card">
+                  <p>{item.productName}</p>
+                  <p>₱{item.productPrice}.00</p>
+                  <button onClick={() => addToCart(item)}>Add To Cart</button>
                 </div>
             )}
           </div>
         </div>
         <div className="cart-part">
           <h2>Shopping Cart</h2>
+          <p className="total">Total items: {totalItems}</p>
+          {cart.map((prod) => {
+            return <div key = {prod.productID} className="cart-item">
+              <div className="items-left">
+                  <p className="item-name">{prod.productName}&nbsp;</p>
+                  <p className="item-price">- ${prod.productPrice}&nbsp;</p>
+              </div>
+              <div className="items-right">
+                  <p className="item-count">x {prod.count}</p>
+                  <button className="delete-button" onClick={() => deleteFromCart(prod)}>X</button>
+              </div>
+            </div>
+          })}
+          <button className="checkout-button">Checkout</button>
         </div>
       </main>
     </>
