d25f6a93f7ab0f346330a62cc5c128078304eff3
nllacabe <nllacabe@up.edu.ph>
2024-05-19 22:32:42 +0800

    ui: minor changes in adminDashboard

--- try-with-react/src/pages/Users.jsx
+++ try-with-react/src/pages/Users.jsx
@@ -25,7 +25,7 @@ const Users = () => {
     return (
         <div className='container'>
         <div className='user-container'>
-            <h2>User Accounts</h2>
+            <h1>User Accounts</h1>
             {loading ? (
                 <p>Loading...</p>
             ) : (
